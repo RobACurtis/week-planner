c21f52aec7706ca8da5d3be02b583af8f2a1e5d1
Dylan De Vei <dylan.devei@gmail.com>
2022-04-08 20:10:36 +0000

    progress.

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 var $addEntry = document.querySelector('.button-1');
 var $popup = document.querySelector('.popup');
 var $overlay = document.querySelector('.overlay');
+var $popup1 = document.querySelector('.container-popup');
 
 $addEntry.addEventListener('click', openPopup);
 
 function openPopup(event) {
-  $popup.className = 'popup margin-popup';
-  $overlay.className = 'overlay';
+  $popup.className = 'popup margin-popup z-index';
+  $overlay.className = 'overlay-on';
+  $popup1.className = 'container-popup z-index';
 }
