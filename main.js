c45067b0ca8f7ac296ca1e9d042bf5c43c78749c
Kyle Parrent <kppp433@gmail.com>
2022-04-08 19:34:06 +0000

    progress.

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 var $addEntry = document.querySelector('.button-1');
 var $popup = document.querySelector('.popup');
+var $overlay = document.querySelector('.overlay');
 
 $addEntry.addEventListener('click', openPopup);
 
 function openPopup(event) {
   $popup.className = 'popup margin-popup';
+  $overlay.className = 'overlay';
 }
